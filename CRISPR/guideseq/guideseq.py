f5ad88d0a2c2550acd96d3de4e624208be2074f1
Richard Michael <rmichael@edgeofthenet.org>
2019-01-29 00:24:55 +0000

    Do not use module aliases for code used once; remove commented code

--- CRISPR/guideseq/guideseq.py
+++ CRISPR/guideseq/guideseq.py
@@ -1,26 +1,24 @@
 import os
 import sys
-import pandas as pd
+import pandas
 
 # replace this with the location of your dsNickFury install
 DSNF_DIRECTORY = "/home/jake/repos/dsNickFury/dsNickFury3PlusOrchid"
 
-pj = os.path.join
 workdir = os.path.dirname(os.path.realpath(__file__))
 os.chdir(DSNF_DIRECTORY)
 sys.path.append("./")
 
 from offtarget_manager import OfftargetManager
 
-
 def get_sequences(fname):
-    df = pd.read_csv(fname)
+    df = pandas.read_csv(fname)
     targets = df['seq'].apply(lambda x: x.split(",")[0][:20]).unique()
     targets = map(lambda x: ("NONE", x.upper(), 'AGG'), targets)
     return targets
 
 if __name__ == "__main__":
-    filename = pj(workdir, "guideseq_unique.txt")
+    filename = os.path.join(workdir, "guideseq_unique.txt")
     om = OfftargetManager(filename, mismatchTolerance=6, endClip=0, mismatchLimit=999999999)
 
     targets = get_sequences(filename)
@@ -32,5 +30,3 @@ if __name__ == "__main__":
 
     om.merge_output_files()
     om.write_hdf5()
-
-    # print om.get_hdf5().columns
